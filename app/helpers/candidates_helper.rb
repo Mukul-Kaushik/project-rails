4cc6087c00acc237ed540fdc38b54ba0d38a2016
mukul-kaushik <mukulkaushik908@gmail.com>
2019-07-10 13:01:59 +0530

    removed unused helper methods

--- app/helpers/candidates_helper.rb
+++ app/helpers/candidates_helper.rb
@@ -3,53 +3,6 @@
 # Candidate Helper
 module CandidatesHelper
 
-  def states_select
-    select_tag :state,options_for_select( ["Andhra Pradesh",
-    "Arunachal Pradesh","
-    Assam",
-    "Bihar",
-    "Chhattisgarh",
-    "Chandigarh",
-    "Dadra and Nagar Haveli",
-    "Daman and Diu",
-    "Delhi",
-    "Goa",
-    "Gujarat",
-    "Haryana",
-    "Himachal Pradesh",
-    "Jammu and Kashmir",
-    "Jharkhand",
-    "Karnataka",
-    "Kerala",
-    "Madhya Pradesh",
-    "Maharashtra",
-    "Manipur",
-    "Meghalaya",
-    "Mizoram",
-    "Nagaland",
-    "Orissa",
-    "Punjab",
-    "Pondicherry",
-    "Rajasthan",
-    "Sikkim",
-    "Tamil Nadu",
-    "Tripura",
-    "Uttar Pradesh",
-    "Uttarakhand",
-    "West Bengal"
-    ]),include_blank:"State",class: "form-control"
-  end
-
-  def status_select
-    select_tag :status,options_for_select(["Pending",
-    "Joined",
-    "Joining",
-    "Studying",
-    "Not interested",
-    "Not responding"
-    ]),include_blank:"Status",class: "form-control"
-  end
-
   def custom_days_select
     select_tag :custom_day,options_for_select([
     "Greater than 365 days",
